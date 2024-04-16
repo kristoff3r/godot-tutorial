1a4d49ee772badd8c0efb85d3637d789515f6a82
Kristoffer Søholm <k.soeholm@gmail.com>
2024-04-16 23:08:15 +0200

    Tutorial done

--- Mob.gd
+++ Mob.gd
@@ -1,35 +1,39 @@
 extends CharacterBody3D
 
-# Emitted when the player jumped on the mob.
 signal squashed
 
-## Minimum speed of the mob in meters per second.
-@export var min_speed = 10
-## Maximum speed of the mob in meters per second.
-@export var max_speed = 18
+# Minimum speed of the mob in meters per second.
+@export var min_speed: int = 10
+# Maximum speed of the mob in meters per second.
+@export var max_speed: int = 18
 
+# This function will be called from the Main scene.
+func initialize(start_position, player_position):
+	# We position the mob by placing it at start_position
+	# and rotate it towards player_position, so it looks at the player.
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	# Rotate this mob randomly within range of -45 and +45 degrees,
+	# so that it doesn't move directly towards the player.
+	rotate_y(randf_range( - PI / 4, PI / 4))
+
+	# We calculate a random speed (integer)
+	var random_speed = randi_range(min_speed, max_speed)
+	# We calculate a forward velocity that represents the speed.
+	velocity = Vector3.FORWARD * random_speed
+	# We then rotate the velocity vector based on the mob's Y rotation
+	# in order to move in the direction the mob is looking.
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+	@warning_ignore("integer_division")
+	$AnimationPlayer.speed_scale = random_speed / min_speed
 
 func _physics_process(_delta):
 	move_and_slide()
 
-
-func initialize(start_position, player_position):
-	look_at_from_position(start_position, player_position, Vector3.UP)
-	rotate_y(randf_range(-PI / 4, PI / 4))
-
-	var random_speed = randf_range(min_speed, max_speed)
-	# We calculate a forward velocity first, which represents the speed.
-	velocity = Vector3.FORWARD * random_speed
-	# We then rotate the vector based on the mob's Y rotation to move in the direction it's looking.
-	velocity = velocity.rotated(Vector3.UP, rotation.y)
-
-	$AnimationPlayer.speed_scale = random_speed / min_speed
-
-
-func squash():
-	squashed.emit()
+func _on_visible_on_screen_notifier_3d_screen_exited():
 	queue_free()
 
-
-func _on_visible_on_screen_notifier_screen_exited():
+## squishy squash
+func squash():
+	squashed.emit()
 	queue_free()
--- Player.gd
+++ Player.gd
@@ -1,19 +1,22 @@
 extends CharacterBody3D
 
-signal hit
-
-## How fast the player moves in meters per second.
+# How fast the player moves in meters per second.
 @export var speed = 14
-## Vertical impulse applied to the character upon jumping in meters per second.
-@export var jump_impulse = 20
-## Vertical impulse applied to the character upon bouncing over a mob in meters per second.
-@export var bounce_impulse = 16
-## The downward acceleration when in the air, in meters per second.
+# The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
+@export var jump_impulse = 20
+@export var bounce_impulse = 16
+
+signal hit
+
+var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
+	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
+
+	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -24,51 +27,53 @@ func _physics_process(delta):
 		direction.z -= 1
 
 	if direction != Vector3.ZERO:
-		# In the lines below, we turn the character when moving and make the animation play faster.
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
-		basis = Basis.looking_at(direction)
+		$Pivot.basis = Basis.looking_at(direction)
 		$AnimationPlayer.speed_scale = 4
 	else:
 		$AnimationPlayer.speed_scale = 1
 
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
 
-	# Jumping.
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y += jump_impulse
+		target_velocity.y = jump_impulse
 
-	# We apply gravity every frame so the character always collides with the ground when moving.
-	# This is necessary for the is_on_floor() function to work as a body can always detect
-	# the floor, walls, etc. when a collision happens the same frame.
-	velocity.y -= fall_acceleration * delta
-	move_and_slide()
-
-	# Here, we check if we landed on top of a mob and if so, we kill it and bounce.
-	# With move_and_slide(), Godot makes the body move sometimes multiple times in a row to
-	# smooth out the character's motion. So we have to loop over all collisions that may have
-	# happened.
-	# If there are no "slides" this frame, the loop below won't run.
+	# Iterate through all collisions that occurred this frame
 	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
 		var collision = get_slide_collision(index)
+
+		# If the collision is with ground
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a mob
 		if collision.get_collider().is_in_group("mob"):
 			var mob = collision.get_collider()
+			# we check that we are hitting it from above.
 			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				# If so, we squash it and bounce.
 				mob.squash()
-				velocity.y = bounce_impulse
-				# Prevent this block from running more than once,
-				# which would award the player more than 1 point for squashing a single mob.
+				target_velocity.y = bounce_impulse
+				# Prevent further duplicate calls.
 				break
 
-	# This makes the character follow a nice arc when jumping
-	rotation.x = PI / 6 * velocity.y / jump_impulse
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
 
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 
 func die():
 	hit.emit()
 	queue_free()
 
-
-func _on_MobDetector_body_entered(_body):
+func _on_mob_detector_body_entered(_body):
 	die()
